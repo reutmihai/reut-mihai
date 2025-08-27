36a9cf03872d1694abeb2085f6544bfe96ea9165
Mihai Reuț <mihaita@rgN.local>
2025-08-27 15:27:39 +0300

    make navbar fixed

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import '../index.css';
 
 const Navbar: React.FC = () => {
   return (
-    <div className="flex justify-center items-center mt-3">
+    <div className="flex justify-center fixed items-center mt-3">
     <nav className="py-3 px-15 flex justify-center items-center gap-10 border-b-[2px] border-[var(--color-purple)]">
       <NavLink 
         to="/" 
