284cdb2eacacb5215796616d7e4c275d641305e2
Mihai Reuț <mihaita@rgN.local>
2025-08-27 13:29:22 +0300

    improve responsive layout and move typewriter animation to desktop only

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,17 +1,14 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-
-
-
 export const Homepage: React.FC = () => {
   const navigate = useNavigate();
   return (
-    <main className="max-w-6xl mx-auto px-6 py-16 flex flex-row gap-20 h-screen place-content-around my-10">
+    <main className="px-3 my-15 flex flex-col-reverse  sm:min-h-screen sm:max-w-6xl sm:mx-auto sm:flex-row sm:items-center sm:justify-between ">
       <div className="card flex flex-col items-center justify-center">
         <div className="typewriter">
           <h1 className="typewriter">
-            Hi there! I'm{" "}
+            Hello there! I am{" "}
             <span className="text-[var(--color-purple)]">Reuț Mihai</span>.{" "}
           </h1>
         </div>
@@ -26,20 +23,18 @@ export const Homepage: React.FC = () => {
             href={`${import.meta.env.BASE_URL}documents/reut_mihai_cv.pdf`}
             target="_blank"
             rel="noopener noreferrer"
-            className="border-[1px] text-[var(--color-purple)] px-4 py-2 rounded"
+            className="px-1 py-1 border-[1px] text-[var(--color-purple)] sm:px-4 sm:py-2 rounded"
           >
             My Resume
           </a>
-          <button className="btn" onClick={() => navigate("projects")}>
-            My Projects
-          </button>
+          <button onClick={() => navigate("projects")}>My Projects</button>
         </div>
       </div>
-      <div className="flex flex-col justify-center items-center">
+      <div className="flex justify-center items-center">
         <img
-           src={`${import.meta.env.BASE_URL}images/profile_home.jpg`}
+          src={`${import.meta.env.BASE_URL}images/profile_home.jpg`}
           alt="Mihai - Web Developer"
-          className="w-[300px] h-[500px] object-cover rounded-xl shadow-lg border border-gray-300 transition-transform duration-500 ease-in-out hover:scale-105"
+          className="w-[200px] h-auto md:w-[300px] h-[500px] object-cover rounded-xl shadow-lg border border-gray-300 transition-transform duration-500 ease-in-out hover:scale-105"
         />
       </div>
     </main>
